827429c0931aea698a9bcd1d14447501afe007c5
marcelinobudi <marcelinobprakasya@gmail.com>
2026-09-16 13:01:41 +0700

    setup .c module

---
+++ src/matrix.c
@@ -0,0 +1,26 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+/*
+Menghitung nilai determinan matrix 3x3
+*/
+int32_t determinan(int32_t matrix[3][3]){
+    int32_t result = 0;
+    return result;
+}
+
+/*
+Menghitung invers dari suatu matrix
+jika fungsi mengembalikan -1 => error.
+*/
+int inverse_matrix(int32_t matrix[3][3], int32_t inversed[3][3]){
+    return 0;
+}
+
+
+/*
+Menentukan kesamaan matrix
+*/
+bool is_matrix_equal(int32_t matrix1[3][3], int32_t matrix2[3][3]) {
+    return true;
+}
---
+++ src/stringx.c
@@ -0,0 +1,38 @@
+#include <string.h>
+
+/*
+stringx -> String Extended
+*/
+
+/*
+Menghitung jumlah karakter dari text
+*/
+size_t character_len(char *text) {
+    size_t cnt = 0;
+    return cnt;
+}
+
+/*
+Menghitung jumlah kata dari text
+*/
+size_t word_len(char *text) {
+    size_t cnt = 0;
+    return cnt;
+}
+
+/*
+Membalikkan string
+kembalian = -1 => error
+*/
+int reverse_string(char *text, char* output) {
+    return 0;
+}
+
+/*
+Mengembalikan string tanpa huruf vokal dari string
+kembalian = -1 => error
+*/
+
+int string_without_vocal_alphabet(char *text, char *output) {
+    return 0;
+}
