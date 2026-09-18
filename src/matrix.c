73c4c8b3d6bc27d086c01a44b63d5bc46354c349
ayla <ayyieela@gmail.com>
2026-09-18 14:37:22 +0700

    Update matrix.c

--- src/matrix.c
+++ src/matrix.c
@@ -4,10 +4,26 @@
 /*
 Menghitung nilai determinan matrix 3x3
 */
-int32_t determinan(int32_t matrix[3][3]){
+int32_t determinan(int32_t matrix[3][3]) {
     int32_t result = 0;
+
+    result =
+        matrix[0][0] * (
+            matrix[1][1] * matrix[2][2] -
+            matrix[1][2] * matrix[2][1]
+        )
+        - matrix[0][1] * (
+            matrix[1][0] * matrix[2][2] -
+            matrix[1][2] * matrix[2][0]
+        )
+        + matrix[0][2] * (
+            matrix[1][0] * matrix[2][1] -
+            matrix[1][1] * matrix[2][0]
+        );
+
     return result;
 }
+
 
 /*
 Menghitung invers dari suatu matrix
