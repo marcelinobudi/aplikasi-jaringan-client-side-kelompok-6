1f730b3f9da50a857c60444f6d6cccfbc490a9ae
ayla <ayyieela@gmail.com>
2026-09-18 14:21:40 +0700

    Update stringx.c

--- src/stringx.c
+++ src/stringx.c
@@ -28,11 +28,26 @@ int reverse_string(char *text, char* output) {
     return 0;
 }
 
-/*
-Mengembalikan string tanpa huruf vokal dari string
+
+//Mengembalikan string tanpa huruf vokal dari string
 kembalian = -1 => error
-*/
 
 int string_without_vocal_alphabet(char *text, char *output) {
+    size_t j = 0;
+
+    for (size_t i = 0; text[i] != '\0'; i++) {
+
+        if (text[i] != 'a' && text[i] != 'i' && text[i] != 'u' &&
+            text[i] != 'e' && text[i] != 'o' &&
+            text[i] != 'A' && text[i] != 'I' && text[i] != 'U' &&
+            text[i] != 'E' && text[i] != 'O') {
+
+            output[j] = text[i];
+            j++;
+        }
+    }
+
+    output[j] = '\0';
+
     return 0;
 }
