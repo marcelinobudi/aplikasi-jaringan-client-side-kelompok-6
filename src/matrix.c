af93d193d4652b9477fc8d733c114f618ac08e8c
ayla <ayyieela@gmail.com>
2026-09-18 14:16:41 +0700

    Update matrix.c

--- src/matrix.c
+++ src/matrix.c
@@ -1,127 +1,85 @@
 #include <stdio.h>
-#include <string.h>
-#include <winsock2.h>
+#include <stdbool.h>
 
-#define BUFFER_SIZE 1024
-
-nye nye nye 
-
-//input matrix 3x3 bagi client
-printf("Masukkan matrix 3x3: \n");
-for (int i=0; i<3; i++){
-    for (int j=0; j<3; j++){
-        printf("M[%d][%d] = ", i + 1, j + 1);
-        scanf("%lf", &matrix[i][j]);
-    }
-}
-
-//menampilkan matrix
-void printMatrix(double matrix[3][3])
-{
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%8.2lf ", matrix[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-//menghitung nilai determinan matrix 
-//sebagai pembanding hasil dari server
-double determinant(double matrix [3][3]) {
-    return
-        matrix[0][0] * (matrix[1][1] * matrix[2][2] -
-                        matrix[1][2] * matrix[2][1])
-      - matrix[0][1] * (matrix[1][0] * matrix[2][2] -
-                        matrix[1][2] * matrix[2][0])
-      + matrix[0][2] * (matrix[1][0] * matrix[2][1] -
-                        matrix[1][1] * matrix[2][0]);
-}
-
-//menghitung invers matrix
-//sebagai pembanding hasil dari server
-int inverseMatrix(double matrix[3][3], double inverse[3][3]){
-    double det = determinant(matrix);
-    if (det == 0) {
-        return 0;
-    }
-    inverse[0][0] =
-        (matrix[1][1] * matrix[2][2] -
-         matrix[1][2] * matrix[2][1]) / det;
-
-    inverse[0][1] =
-        (matrix[0][2] * matrix[2][1] -
-         matrix[0][1] * matrix[2][2]) / det;
-
-    inverse[0][2] =
-        (matrix[0][1] * matrix[1][2] -
-         matrix[0][2] * matrix[1][1]) / det;
-
-    inverse[1][0] =
-        (matrix[1][2] * matrix[2][0] -
-         matrix[1][0] * matrix[2][2]) / det;
-
-    inverse[1][1] =
-        (matrix[0][0] * matrix[2][2] -
-         matrix[0][2] * matrix[2][0]) / det;
-
-    inverse[1][2] =
-        (matrix[0][2] * matrix[1][0] -
-         matrix[0][0] * matrix[1][2]) / det;
-
-    inverse[2][0] =
-        (matrix[1][0] * matrix[2][1] -
-         matrix[1][1] * matrix[2][0]) / det;
-
-    inverse[2][1] =
-        (matrix[0][1] * matrix[2][0] -
-         matrix[0][0] * matrix[2][1]) / det;
-
-    inverse[2][2] =
-        (matrix[0][0] * matrix[1][1] -
-         matrix[0][1] * matrix[1][0]) / det;
-    return 1;
-}
-
-
-//mengirim matrix ke server
-send(clientSocket, (char*)matrix, sizeof(matrix),0);
-printf("\nMatriks berhasil dikirim ke server\n");
-printf("Menunggu hasil dari server...\n");
-
-//menerima hasil dari server dan memeriksa determinan
-int bytesReceived = recv(clientSocket, result, BUFFER_SIZE - 1,0);
-if (bytesReceived>0) {
-    result[bytesReceived] = '\0';
-
-    printf("\n===== HASIL DARI SERVER =====\n");
-    printf("%s\n", result);
-
-} else {
-    printf("\nTidak menerima hasil dari server.\n");
-}
-
-
-/*
-Menghitung nilai determinan matrix 3x3
-*/
-int32_t determinan(int32_t matrix[3][3]){
+//menghitung nilai determinan matrix
+int32_t determinan(int32_t matrix[3][3]) {
     int32_t result = 0;
+
+    result =
+        matrix[0][0] * (
+            matrix[1][1] * matrix[2][2] -
+            matrix[1][2] * matrix[2][1]
+        )
+        - matrix[0][1] * (
+            matrix[1][0] * matrix[2][2] -
+            matrix[1][2] * matrix[2][0]
+        )
+        + matrix[0][2] * (
+            matrix[1][0] * matrix[2][1] -
+            matrix[1][1] * matrix[2][0]
+        );
+
     return result;
 }
 
-/*
-Menghitung invers dari suatu matrix
-jika fungsi mengembalikan -1 => error.
-*/
-int inverse_matrix(int32_t matrix[3][3], int32_t inversed[3][3]){
+//Menghitung invers dari suatu matrix 
+//jika fungsi mengembalikan -1 => error.
+int inverse_matrix(int32_t matrix[3][3], int32_t inversed[3][3]) {
+    int32_t det = determinan(matrix);
+
+    /* Matrix tidak memiliki invers */
+    if (det == 0) {
+        return -1;
+    }
+
+    inversed[0][0] =
+        (matrix[1][1] * matrix[2][2] -
+         matrix[1][2] * matrix[2][1]) / det;
+
+    inversed[0][1] =
+        (matrix[0][2] * matrix[2][1] -
+         matrix[0][1] * matrix[2][2]) / det;
+
+    inversed[0][2] =
+        (matrix[0][1] * matrix[1][2] -
+         matrix[0][2] * matrix[1][1]) / det;
+
+    inversed[1][0] =
+        (matrix[1][2] * matrix[2][0] -
+         matrix[1][0] * matrix[2][2]) / det;
+
+    inversed[1][1] =
+        (matrix[0][0] * matrix[2][2] -
+         matrix[0][2] * matrix[2][0]) / det;
+
+    inversed[1][2] =
+        (matrix[0][2] * matrix[1][0] -
+         matrix[0][0] * matrix[1][2]) / det;
+
+    inversed[2][0] =
+        (matrix[1][0] * matrix[2][1] -
+         matrix[1][1] * matrix[2][0]) / det;
+
+    inversed[2][1] =
+        (matrix[0][1] * matrix[2][0] -
+         matrix[0][0] * matrix[2][1]) / det;
+
+    inversed[2][2] =
+        (matrix[0][0] * matrix[1][1] -
+         matrix[0][1] * matrix[1][0]) / det;
+
     return 0;
 }
 
-
-/*
-Menentukan kesamaan matrix
-*/
+//menentukan kesaaan matrix dari hasil server
 bool is_matrix_equal(int32_t matrix1[3][3], int32_t matrix2[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (matrix1[i][j] != matrix2[i][j]) {
+                return false;
+            }
+        }
+    }
+
     return true;
 }
