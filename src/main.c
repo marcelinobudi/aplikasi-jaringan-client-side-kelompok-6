fd10b3a8656f205448e4cec0e2bbc2dfdbdacd4c
marcelinobudi <marcelinobprakasya@gmail.com>
2026-09-21 10:15:37 +0700

    implement service_server.c on main.c

--- src/main.c
+++ src/main.c
@@ -3,13 +3,140 @@
 #include <string.h>
 #include "client_networks/server_service.h"
 #include "client_networks/client_socket.h"
+#include "client_networks/stringx.h"
+#include "client_networks/matrix.h"
+
+enum ServerService {
+    CHARACTER_COUNT = '1',
+    WORD_COUNT,
+    REVERSE_STRING,
+    STRING_WITHOUT_VOWELS,
+    DETERMINAN_AND_INVERSE_MATRIX
+};
+bool is_service_available(server_service *service, char *service_name) {
+    char *service_availibility = "INACTIVE";
+    service_availibility_check(service, service_name, service_availibility);
+    return (strcmp(service_availibility, "ACTIVE") == 0);
+}
+void character_count(server_service *service) {
+    if(!is_service_available(service, "CHARACTER_COUNT")) {
+        printf("Layanana CHARACTER_COUNT tidak aktif");
+        return;
+    }
+
+    char text[BUFFER_SIZE-10]; // "10 char reserved for 'REQUEST ' and the rest for safety"
+    printf("input string: ");
+    fgets(text, sizeof(text), stdin);
+
+    size_t num;
+    if(service_character_count(service, text, &num) < 0) {
+        return;
+    }
+    
+    bool is_valid = character_len(text) == num;
+    service_acknowledgement(service, is_valid);
+    printf("jumlah karakter (%s): %zu", (is_valid) ? "VALID": "INVALID",num);
+
+}
+void word_count(server_service *service) {
+    if(!is_service_available(service, "WORD_COUNT")) {
+        printf("Layanan WORD_COUNT tidak aktif");
+        return;
+    }
+
+    char text[BUFFER_SIZE-10]; // "10 char reserved for 'REQUEST ' and the rest for safety"
+    printf("input string: ");
+    fgets(text, sizeof(text), stdin);
+
+    size_t num;
+    if(service_word_count(service, text, &num) < 0) {
+        return;
+    }
+    bool is_valid = word_len(text) == num;
+    service_acknowledgement(service, is_valid);
+    printf("jumlah kata (%s): %zu", (is_valid) ? "VALID": "INVALID",num);
+
+}
+void t_reverse_string(server_service *service) {
+    if(!is_service_available(service, "REVERSE_STRING")) {
+        printf("Layanan REVERSE_STRING tidak aktif");
+        return;
+    }
+
+    char text[BUFFER_SIZE-10]; // "10 char reserved for 'REQUEST ' and the rest for safety"
+    printf("input string: ");
+    fgets(text, sizeof(text), stdin);
+
+    char reversed[BUFFER_SIZE];
+    if(service_reverse_string(service, text, reversed) < 0) {
+        return;
+    }
+    char reversed_test[BUFFER_SIZE];
+    if(reverse_string(text, reversed_test) <0) {
+        printf("error: t_reverse_string -> reverse_string");
+        return;
+    }
+    bool is_valid = strcmp(reversed, reversed_test) == 0;
+    service_acknowledgement(service, is_valid);
+    printf("string terbalik (%s): %zu", (is_valid) ? "VALID": "INVALID", reversed);
+
+}
+void string_without_vowels(server_service *service) {
+    if(!is_service_available(service, "STRING_WITHOUT_VOWELS")) {
+        printf("Layanan STRING_WITHOUT_VOWELS tidak aktif");
+        return;
+    }
+
+    char text[BUFFER_SIZE-10]; // "10 char reserved for 'REQUEST ' and the rest for safety"
+    printf("input string: ");
+    fgets(text, sizeof(text), stdin);
+
+    char without_vowels[BUFFER_SIZE];
+    if(service_string_without_vowels(service, text, without_vowels) < 0) {
+        return;
+    }
+    char without_vowel_test[BUFFER_SIZE];
+    if(string_without_vocal_alphabet(text, without_vowel_test) <0) {
+        printf("error: string_without_vowels -> string_without_vocal_alphabets");
+        return;
+    }
+    bool is_valid = strcmp(without_vowels, without_vowel_test) == 0;
+    service_acknowledgement(service, is_valid);
+    printf("string tanpa vokal (%s): %zu", (is_valid) ? "VALID": "INVALID", without_vowels);
+}
+void determinan_and_inverse_matrix(server_service *service) {
+    if(!is_service_available(service, "DETERMINAN_AND_INVERSE_MATRIX")) {
+        printf("Layanan DETERMINAN_AND_INVERSE_MATRIX tidak aktif");
+        return;
+    }
+
+    double matrix[3][3];
+    printf("input matrix: ");
+    if(scanf("%lf %lf %lf %lf %lf %lf %lf %lf %lf", matrix[0][0], matrix[0][1], matrix[0][2], matrix[1][0], matrix[1][1], matrix[1][2], matrix[2][0], matrix[2][1], matrix[2][2]) < 9) {
+        printf("Input tidak valid.");
+        return;
+    }
+
+    double d_determinan;
+    double m_out[3][3];
+    if(service_determinan_and_inverse_matrix(service, matrix, &d_determinan, m_out) < 0) {
+        return;
+    }
+    double determinan_test = determinan(matrix);
+    double m_out_test[3][3];
+    if(inverse_matrix(matrix, m_out_test) < 0 ) {
+        printf("deteriman_and_inverse_matrix -> inverse_matrix");
+        return;
+    }
+
+    bool is_valid = (d_determinan == determinan_test) && (is_matrix_equal(m_out, m_out_test));
+    service_acknowledgement(service, is_valid);
+    char *is_valid_str = (is_valid) ? "VALID": "INVALID";
+    printf("hasil determinan (%s): %lf", is_valid_str, d_determinan);
+    printf("hasil inverse matrix (%s): %lf %lf %lf %lf %lf %lf %lf %lf %lf", is_valid_str, m_out[0][0], m_out[0][1], m_out[0][2], m_out[1][0], m_out[1][1], m_out[1][2], m_out[2][0], m_out[2][1], m_out[2][2]);
+}
 
 int main() {
-    // client_socket client = {};
-    // printf("[+] connecting to server...\n");
-    // if(connect_to_server(&client) < 0) {
-    //     return 1;
-    // }
 
     server_service *service = service_init();
     if(service == NULL) {
@@ -28,29 +155,33 @@ int main() {
             break;
         }
 
-        
-
-        // printf("[+] sending message to server...\n");
-        // if(send_message(&client, user_input) < 0) {
-        //     close_socket(&client);
-        //     return 1;
-        // } 
-
-        // printf("[+] trying to receive message\n");
-        char message[BUFFER_SIZE];
-        // if(receive_message(&client, message) < 0) {
-        //     close_socket(&client);
-        //     return 1;
-        // }
-
-        if(service_testing(service, user_input, message) < 0) {
-            printf("error");
-            return 1;
+        switch(user_input[0]) {
+            case CHARACTER_COUNT: {
+                character_count(service);
+                break;
+            }
+            case WORD_COUNT: {
+                word_count(service);
+                break;
+            }
+            case REVERSE_STRING:{
+                t_reverse_string(service);
+                break;
+            }
+            case STRING_WITHOUT_VOWELS: {
+                string_without_vowels(service);
+                break;
+            }
+            case DETERMINAN_AND_INVERSE_MATRIX: {
+                determinan_and_inverse_matrix(service);
+                break;
+            } 
+            default : {
+                printf("Input tidak valid");
+            }
         }
-        printf("Message: %s\n", message);
     }
     
-
     service_stop(service);
     return 0;
 }
